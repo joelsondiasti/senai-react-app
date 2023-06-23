8a8f69544e5030f2adb9889bb3fd3362f066e116
Joelson Dias <joelsondiasti@outlook.com>
2023-06-23 20:40:13 -0300

    chore: add card of credential

--- src/pages/gitcredential.tsx
+++ src/pages/gitcredential.tsx
@@ -1,6 +1,8 @@
 import { github } from "@/lib/axios";
-import { useState } from "react";
-
+import { CredentialCard, CredentialContainer } from "@/styles/github";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import GithubLogo from "../assets/img/GithubLogo.svg";
 interface GitHubResponse {
   login: string;
   avatar_url: string;
@@ -8,12 +10,25 @@ interface GitHubResponse {
 }
 
 export default function GitCredential() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState<GitHubResponse>({} as GitHubResponse);
 
   async function getGitHubUser(name: string) {
     const { data } = await github.get<GitHubResponse>(name);
     setUser(data);
   }
 
-  return <h1>GitCredential</h1>;
+  useEffect(() => {
+    getGitHubUser("joelsondiasti");
+  }, []);
+
+  return (
+    <CredentialContainer>
+      <CredentialCard>
+        <div>
+          <Image src={GithubLogo} alt="" />
+        </div>
+        <h1>{user.name}</h1>
+      </CredentialCard>
+    </CredentialContainer>
+  );
 }
--- src/styles/github.ts
+++ src/styles/github.ts
@@ -1,11 +1,22 @@
 import { styled } from "@stitches/react";
 
-export const Avatar = styled("img", {
-    width: "120px",
-  });
-  
-export const Bio = styled("span", {
-    display: "block",
-    fontSize: "0.75rem",
-    color: "red",
-  });
+export const CredentialContainer = styled("div", {
+  width: "100vw",
+  height: "100vh",
+  background: "linear-gradient(120deg, #FFF 0%, #87e4f7 100%)",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  fontFamily: "'Noto Sans', sans-serif",
+});
+
+export const CredentialCard = styled("main", {
+  backgroundColor: "#1e1e1e",
+  borderRadius: "24px",
+  width: "360px",
+  height: "490px",
+  boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  color: "#fff",
+});
